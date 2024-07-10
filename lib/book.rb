fc9f7f76fdf00145634e0d45a7f52ac5cd52d4df
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-10 17:12:48 -0400

    Book Class

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_accessor :author, :genre, :page_count, :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
